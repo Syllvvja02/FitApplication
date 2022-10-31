b261fc1f4718922981db024ce67750a57cfff3d0
Syllvvja02 <mikolajczykdrazeksylwia@gmail.com>
2022-10-31 12:33:51 +0100

    without secret key

--- djangoProject/djangoProject/settings.py
+++ djangoProject/djangoProject/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nyfx%iwah-@*%nliduh2+bhcx#^wwn1)mj1yx8*#_^+65(a*g_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
